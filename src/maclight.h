e6b8dfbf456dd6f90b00e71a5082e3da71126807
Matthew Potter <matthew@synapseware.com>
2014-08-27 22:44:42 -0700

    Initial commit of empty project with media files

---
+++ src/maclight.h
@@ -0,0 +1,12 @@
+#ifndef _MACLIGHT_H_
+#define _MACLIGHT_H_
+
+#include <avr/io.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+
+
+// ---------------------------------------------------------------------------------
+// PORTSB & D are used for output
+
+#endif
