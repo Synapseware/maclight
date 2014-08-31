440182d383694eb47850a8767606d2c16b892d0c
Matthew Potter <matthew@synapseware.com>
2014-08-31 01:41:59 -0700

    Matrix resolving correctly

--- src/maclight.c
+++ src/maclight.c
@@ -1,32 +1,119 @@
 #include "maclight.h"
 
-
-
-
-
-
-
-
+volatile uint8_t	_tick		= 0;
+volatile uint16_t	_delay		= 0;
 
 
 // ---------------------------------------------------------------------------------
-// Prepare the board
-void init(void)
+// Sets up Timer1 to count in 1MS ticks
+void setupMSTimer(void)
 {
+	TCCR1	=	(1<<CTC1)	|		// Clear on OCR1C match
+				(0<<PWM1A)	|
+				(0<<COM1A1)	|
+				(0<<COM1A0)	|
+				(0<<CS13)	|		// CLK/64
+				(1<<CS12)	|
+				(1<<CS11)	|
+				(1<<CS10);
 
+	GTCCR	|=	(0<<PWM1B)	|
+				(0<<COM1B1)	|
+				(0<<COM1B0)	|
+				(0<<FOC1B)	|
+				(0<<FOC1A)	|
+				(0<<PSR1);
+
+	TIMSK	|=	(1<<OCIE1A)	|
+				(0<<OCIE1B)	|
+				(0<<TOIE1);
+
+	PLLCSR	=	(0<<LSM)	|
+				(0<<PCKE)	|
+				(0<<PLLE)	|
+				(0<<PLOCK);
+
+	OCR1C	=	249;
 }
 
+// ---------------------------------------------------------------------------------
+// Prepare the board
+void setup(void)
+{
+	setupMSTimer();
+
+	// setup the LED output port
+	DDRB	|=	(1<<LED_IO); 		// LED debug pin
+
+	sei();
+}
+
+// ---------------------------------------------------------------------------------
+// Toggles the LED pin
+inline void toggleLED(void)
+{
+
+	PINB |= (1<<LED_IO);
+}
+
+// ---------------------------------------------------------------------------------
+// Test function to turn on a specific LED
+void toggleMatrixLed(uint8_t index)
+{
+	if (index > CHALEY_PLEX_MATRIX_LEN - 1)
+		return;
+
+	uint8_t
+		sel,
+		dir,
+		mtx		= pgm_read_byte(&CHALEY_PLEX_MATRIX[index]);
+
+	sel = (mtx >> 4) & 0x0F;
+	dir = mtx & 0x0F;
+
+	DDRB	= dir | (DDRB & 0xF0);
+	PORTB	= sel | (PORTB & 0xF0);
+}
+
+// ---------------------------------------------------------------------------------
+// cycles through the 12 LEDs
+void cycleMatrixLed(void)
+{
+	static uint8_t led = 0;
+	toggleMatrixLed(led);
+	if (led++ > CHALEY_PLEX_MATRIX_LEN - 1)
+		led = 0;
+}
 
 // ---------------------------------------------------------------------------------
 // Main
 int main(void)
 {
-    init();
+	setup();
 
-    while(1)
-    {
-        // do events
-    }
+	while(1)
+	{
+		// do events
+		if (_tick)
+		{
+			toggleLED();
 
-    return 0;
+			cycleMatrixLed();
+			
+			_tick = 0;
+		}
+	}
+
+	return 0;
+}
+
+// ---------------------------------------------------------------------------------
+// Timer0 compare A interrupt handler
+ISR(TIM1_COMPA_vect)
+{
+	if (_delay++ > DELAY - 1)
+	{
+		_delay = 0;
+		_tick = 1;
+	}
 }
--- src/maclight.h
+++ src/maclight.h
@@ -2,11 +2,42 @@
 #define _MACLIGHT_H_
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <avr/power.h>
 #include <avr/sleep.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
 
 
 // ---------------------------------------------------------------------------------
 // PORTSB & D are used for output
+
+
+#define DELAY			50
+
+#define LED_IO          PB4
+
+
+// define the charley plex data for each LED
+// 12 LEDs in total
+
+// high nibble is LED selection, low nibble is port direction
+const static uint8_t CHALEY_PLEX_MATRIX[] PROGMEM = {
+    0b00010011,				// 1
+    0b00100011,				// 2
+    0b00100110,				// 3
+    0b01000110,				// 4
+    0b01001100,				// 5
+    0b10001100,				// 6
+    0b00010101,				// 7
+    0b01000101,				// 8
+    0b00101010,				// 9
+    0b10001010,				// 10
+    0b00011001,				// 11
+    0b10001001				// 12
+};
+static const uint16_t CHALEY_PLEX_MATRIX_LEN = sizeof(CHALEY_PLEX_MATRIX)/sizeof(uint8_t);
+
+
 
 #endif
